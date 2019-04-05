a710f589a3c4d1142b5fe10659445e795b727543
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-04-05 23:02:47 +0400

    sorting

--- SortingAlgorithms/SortingAlgorithms/Program.cs
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -4,10 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Bublesort;
-using Heapsort;
-using Insertionsort;
-using Mergesort;
+
 
 
 
