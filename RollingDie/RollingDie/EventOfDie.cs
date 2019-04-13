a6bb2980d5583e1542a4e57603e5e0a5ed3401b4
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-04-13 20:06:07 +0400

    RollingDie

---
+++ RollingDie/RollingDie/EventOfDie.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollingDie
+{
+    class EventOfDie
+    {        
+        public delegate void Rollingdie(int contant);
+        public event Rollingdie TwoFoursInRow; // The row has two 4 number
+        public event Rollingdie SumOfNumber; // The sum of numbers
+
+        public EventOfDie(int[] die)
+        {            
+            Random random = new Random();
+            for (int j = 0; j < die.Length; j++)
+            {
+                die[j] = random.Next(1, 7);
+            }
+                Print(die);            
+        }
+         public void Events(int[] die)
+         {            
+
+            if (TwoFoursInRow != null && TwoNumbers(die) == 2)
+            {
+                this.TwoFoursInRow(TwoNumbers(die));
+            }
+
+            if (SumOfNumber != null && Sum(die) >= 20)
+            {
+                this.SumOfNumber(Sum(die));
+            }
+         }
+
+        /// <summary>
+        /// Check if die shows two fours in a row
+        /// </summary>
+        /// <param name="arr">Die</param>
+        /// <returns></returns>
+        private int TwoNumbers(int[] die)
+        {
+            int count = 0;
+            for (int i = 0; i < die.Length; i++)
+            {
+                if (die[i]==4)
+                {
+                    count++;
+                }
+            }
+            return count;     
+        }
+
+        /// <summary>
+        /// Count summary of the numbers
+        /// </summary>
+        /// <param name="die">Die</param>
+        /// <returns></returns>
+        private int Sum(int[] die)
+        {
+            int sum = 0;
+            for (int i = 0; i < die.Length; i++)
+            {
+                sum += die[i];
+            }
+            return sum;
+        }
+
+        public void Print(int[] die)
+        {
+            for (int i = 0; i < die.Length; i++)
+            {
+                Console.Write(die[i] + " ");
+            }
+        }
+
+    }
+}
---
+++ RollingDie/RollingDie/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollingDie
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 50; i++)
+            {
+                Console.WriteLine("\nToss {0}", i);
+                int[] contant = new int[5];
+                EventOfDie die = new EventOfDie(contant);
+                die.SumOfNumber += Die_SumOfNumber;
+                die.TwoFoursInRow += Die_TwoFoursInRow;
+
+                die.Events(contant);
+            }         
+        }
+        private static void Die_TwoFoursInRow(int count)
+        {
+            Console.Write("\nTwo fours in a row ");
+        }
+
+        private static void Die_SumOfNumber(int sum)
+        {
+            Console.WriteLine("\n{0} is greater than 20 ", sum);
+            
+        }
+    }
+}
