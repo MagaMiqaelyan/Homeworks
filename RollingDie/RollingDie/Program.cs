a534069da0361fc643a6db44dba20af3d354f507
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-04-16 23:06:26 +0400

    RollingDie

--- RollingDie/RollingDie/Program.cs
+++ RollingDie/RollingDie/Program.cs
@@ -19,6 +19,7 @@ namespace RollingDie
         /// <summary>
         /// Attend an event that raise if there are two fours in a row
         /// </summary>
+        /// <param name="count">Fours number count</param>
         private static void Die_TwoFoursInRow()
         {
             Console.WriteLine("\nTwo fours in a row ");
@@ -27,6 +28,7 @@ namespace RollingDie
         /// <summary>
         /// Attend an event that raise if the summary greater 20
         /// </summary>
+        /// <param name="sum"></param>
         private static void Die_SumOfNumber()
         {
             Console.WriteLine("\nSummary is greater than 20 or equal 20");
