25e05c9b07641332877b280bb7fd70d68962f575
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-05-07 18:44:09 +0400

    exceptions

---
+++ Exceptions/Exceptions/Client.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public  class Client
+    {
+        public string Name { get; set; }
+        public int Id { get; private set; }
+        public Dictionary<int, string> clients = new Dictionary<int, string>();
+
+        /// <summary>
+        /// Parameterless constructor
+        /// </summary>
+        public Client()
+        {
+                
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"> A new client name </param>
+        /// <param name="id"> A new client id </param>
+        public Client(int id, string name)
+        {
+            this.Name = name;
+            this.Id = id;
+            this.clients.Add(id, name);
+        }
+
+        /// <summary>
+        /// Add client
+        /// </summary>
+        /// <param name="name"> Client name </param>
+        /// <param name="id"> Client id </param>
+        public void Add(int id, string name)
+        {
+            if (!clients.Keys.Contains(id))
+            {
+                clients.Add(id, name);
+            }
+            else
+            {
+                throw new ClientException("Your client id is wrong");
+            }
+           
+        }
+
+        /// <summary>
+        /// Find client by id
+        /// </summary>
+        /// <param name="id"> Client id </param>
+        /// <returns> Client name </returns>
+        public string FindClient(int id)
+        {
+            if (clients.Keys.Contains(id))
+            {
+                return clients[id];
+            }
+            else
+            {
+                throw new ClientException("Unable to find a client");
+            }
+        }
+    }
+}
---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            //AddElement(arr, 8); // IndexOutOfRangeException
+            // Division(4, 0); // DivideByZeroException
+
+            // My exception
+            Client client = new Client(10, "Anna");
+            client.Add(15, "Armen");
+            Console.WriteLine(client.FindClient(15));
+            //Console.WriteLine(client.FindClient(12));
+            client.Add(10, "Tigran");
+        }
+
+
+        /// <summary>
+        /// Add elements in the array
+        /// </summary>
+        /// <param name="array"> Array </param>
+        /// <param name="count"> Elements count </param>
+        public static void AddElement(int[] array, int count)
+        {
+            Random random = new Random();
+            try
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    array[i] = random.Next(1, 10);
+                }               
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw ex;                
+            }
+        }
+
+        /// <summary>
+        /// Divide two numbers
+        /// </summary>
+        /// <param name="num1"> Divided </param>
+        /// <param name="num2"> Divisor </param>
+        public static void Division(int num1, int num2)
+        {
+            int result = 0;
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("Result: {0}", result);
+            }
+        }
+    }
+    
+}
