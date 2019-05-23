9a7ff9bbb1558cd40a8294844e0588438702ac91
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-05-23 19:47:23 +0400

    copy file

---
+++ CopyFile/CopyFile/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\User\Desktop\Maga";
+            string fileName = "Maga.docx";
+            string fileNameCopy = "MagaCopy.docx";
+
+            string sourceFile = Path.Combine(path, fileName);
+            string destFile = Path.Combine(path, fileNameCopy);
+
+            Console.WriteLine("Starting copy... ");
+            CopyPercent(sourceFile, destFile);
+            Console.WriteLine("The end !!");
+        }
+
+        /// <summary>
+        /// Copy file
+        /// </summary>
+        /// <param name="soureceFile"> Source file </param>
+        /// <param name="destFile"> Copy file </param>
+        static public async void CopyPercent(string soureceFile, string destFile)
+        {
+            using (FileStream readStream = File.OpenRead(soureceFile))
+            using (FileStream writeStream = File.OpenWrite(destFile))
+            {
+                byte[] buffer = new byte[1 * 1];
+                int bytesRead;
+                string old = "0";
+                
+                while ((bytesRead = readStream.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    writeStream.Write(buffer, 0, bytesRead);
+                    string progress = await Percentage(writeStream.Length, readStream.Length);
+                    
+                    if (progress != old)
+                    {
+                        Console.WriteLine("Progress: {0} %", progress);
+                        old = progress;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Calculate how many percent have been copied
+        /// </summary>
+        /// <param name="writeStreamLength"> Source file size</param>
+        /// <param name="readStreamLength"> Current file size</param>
+        /// <returns></returns>
+        static async private Task<string> Percentage(long writeStreamLength, long readStreamLength)
+        {
+            await Task.Delay(0);
+            return ((writeStreamLength * 100) / readStreamLength).ToString();        
+        }
+    }
+}
