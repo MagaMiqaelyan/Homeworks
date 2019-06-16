335bc6bc6ec24fad163dee2019073af97a60eb10
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-06-16 16:40:30 +0400

    department

---
+++ WcfServiceDepartment/DepClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using WcfServiceDepartment;
+
+namespace DepClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceDep client = new ServiceDep();
+            List<Department> dep = client.GetDeparmentNames("Research and Development");
+            if (dep.Count == 0)
+            {
+                Console.WriteLine("Something is wrong");
+            }
+            else
+            {
+                foreach (var item in dep)
+                {
+                    Console.WriteLine("{0}\t{1}\t{2}", item.DepartmentId, item.Name, item.GroupName);
+                }
+            }
+            //Department department = new Department()
+            //{
+            //    DepartmentId = 17,
+            //    Name = "Finance",
+            //    GroupName = "Sales and Marketing"
+            //};
+            //Console.WriteLine(client.InsertDeparment(department));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WcfServiceDepartment/WcfServiceDepartment/IServiceDep.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WcfServiceDepartment
+{   
+    [ServiceContract]
+    public interface IServiceDep
+    {
+        [OperationContract]
+        List<Department> GetDeparmentNames(string groupName);
+        //string InsertDeparment(Department department);
+
+    }
+        
+    [DataContract]
+    public class Department
+    {        
+        [DataMember]
+        public int DepartmentId { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string GroupName { get; set; }
+    }
+}
