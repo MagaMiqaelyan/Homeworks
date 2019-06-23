1ec7723b5960ef4be19215e4724cb85f29aed5a9
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-06-23 16:02:58 +0400

    blockchain

---
+++ ProofOfWork/ProofOfWork/BlockchainExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace ProofOfWork
+{
+    public static class BlockchainExtensions
+    {
+        /// <summary>
+        /// Check the validity of the chain, to be sure that the chain was not hacked, was not tampered. 
+        /// </summary>
+        /// <returns> False if the block was modified or tampered, otherwise true.</returns>
+        public static bool IsValid(IList<Block> Chain)
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ ProofOfWork/ProofOfWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ProofOfWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startTime = DateTime.Now;
+
+            Blockchain phillyCoin = new Blockchain();
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Henry,receiver:MaHesh,amount:10}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
+            phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Mahesh,receiver:Henry,amount:5}"));
+
+            var endTime = DateTime.Now;
+
+            Console.WriteLine($"Duration: {endTime - startTime}");
+            Console.WriteLine(JsonConvert.SerializeObject(phillyCoin, Formatting.Indented));
+        }
+    }
+}
